752e4f2b2fb9bd4863f662bd20324e9bb7a07c16
SachinChauhanPy <devlopersachin007@gmail.com>
2024-12-22 13:12:37 +0530

    Configure settings  - Load secrets from .env  - Add rest_framework and rest_framework_api_key to installed apps

--- vetty_apis/settings.py
+++ vetty_apis/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i)vc#nh!u6a84g66ivnw+8t8*dah9f@b43bxeq@nv*@0vd8iv%'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third Party Apps
+    'rest_framework',
+    "rest_framework_api_key",
+    # Custom Apps
 ]
 
 MIDDLEWARE = [
